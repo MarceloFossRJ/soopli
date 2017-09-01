ee36bb7187efa00f7194e7b9086df4c405d5cb0b
marcelofossrj <marcelo.foss.rj@gmail.com>
2017-09-01 18:48:13 -0300

    association person-position

--- app/models/soopli/person.rb
+++ app/models/soopli/person.rb
@@ -2,6 +2,8 @@ module Soopli
   class Person < ApplicationRecord
     has_paper_trail class_name: 'Soopli::PersonVersion'
     
+    belongs_to :position
+    
     validates :name, presence: true, length: {in: 3..250}
     validates :email, uniqueness: true, format: { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, 
               message: "Email not valid" }, allow_blank: true
--- app/models/soopli/position.rb
+++ app/models/soopli/position.rb
@@ -2,6 +2,8 @@ module Soopli
   class Position < ApplicationRecord
     has_paper_trail class_name: 'Soopli::PositionVersion'
     
+    has_many :people
+    
     validates :name, presence: true, length: {in: 1..250}, uniqueness: true
 
   end
